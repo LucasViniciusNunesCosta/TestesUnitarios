ae6d43eb9eadd0eb16517b9bbb019f9c34bf52df
Lucas Vinicius <Lucasvinicius3105@gmail.com>
2021-09-25 01:45:37 -0300

    Teste GerenciamentoClientes quase completo

--- testes/negocio/GerenciadoraClientestest.java
+++ testes/negocio/GerenciadoraClientestest.java
@@ -20,6 +20,7 @@ public class GerenciadoraClientestest {
 	private GerenciadoraClientes Gclientes;
 	private int idCliente1 = 1;
 	private int idCliente2 = 2;
+	private int idCliente3 = 3;
 	
 	@Before
 	public void cenario() {
@@ -27,6 +28,7 @@ public class GerenciadoraClientestest {
 		//criacao de clientes ficticios
 		Cliente cliente1 = new Cliente(idCliente1, "Gian", 26, "GianManincor@gmail.com", 21, true);
 		Cliente cliente2 = new Cliente(idCliente2, "Lucas", 19, "LucasVinicius@gmail.com", 20, true);
+		Cliente cliente3 = new Cliente(idCliente3, "Pedro", 24, "Pedro@gmail.com", 22, true);
 		
 		//inserindo os clientes ficticios na lista de clientes do banco
 		List<Cliente> ClientesBanco = new ArrayList<Cliente>();
@@ -46,5 +48,26 @@ public class GerenciadoraClientestest {
 		Cliente cliente = Gclientes.pesquisaCliente(idCliente1);
 		
 		assertThat(cliente.getId(), Is.is(idCliente1));
+	}
+	
+	@Test
+	public void deveRemoverClientePorId() {
+		boolean clienteRemovido = Gclientes.removeCliente(idCliente2);
+		
+		assertThat(clienteRemovido, Is.is(true));
+		assertThat(Gclientes.getClientesDoBanco().size(), Is.is(1));
+		assertNull(Gclientes.pesquisaCliente(idCliente2));
+	}
+	
+	@Test
+	public void deveVerificarSeClienteEstaAtivo() {
+		boolean clienteAtivo = Gclientes.clienteAtivo(idCliente1);
+		
+		assertThat(clienteAtivo, Is.is(true));
+		assertThat(Gclientes.getClientesDoBanco().size(), Is.is(2));
+	}
+	@Test
+	public void deveAdicionarNovoCliente() {
+		
 	}
 }

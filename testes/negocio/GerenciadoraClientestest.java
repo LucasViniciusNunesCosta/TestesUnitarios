89454a6b4719ca55bbdc4f20725e9b016bd96a65
Gian de Manincor <gianmanincor@gmail.com>
2021-09-25 19:35:27 -0300

    finalizado teste GerenciadoraCliente

--- testes/negocio/GerenciadoraClientestest.java
+++ testes/negocio/GerenciadoraClientestest.java
@@ -20,18 +20,24 @@ public class GerenciadoraClientestest {
 	private GerenciadoraClientes Gclientes;
 	private int idCliente1 = 1;
 	private int idCliente2 = 2;
+	private int idCliente3 = 3;
+	private int idCliente4 = 4;
 	
 	@Before
 	public void cenario() {
 		
 		//criacao de clientes ficticios
 		Cliente cliente1 = new Cliente(idCliente1, "Gian", 26, "GianManincor@gmail.com", 21, true);
-		Cliente cliente2 = new Cliente(idCliente2, "Lucas", 19, "LucasVinicius@gmail.com", 20, true);
+		Cliente cliente2 = new Cliente(idCliente2, "Lucas", 9, "LucasVinicius@gmail.com", 20, true);
+		Cliente cliente3 = new Cliente(idCliente3, "Pedro", 20, "Pedro@gmail.com", 19, true);
+		Cliente cliente4 = new Cliente(idCliente4, "Pedro", 80, "Pedro@gmail.com", 19, true);
 		
 		//inserindo os clientes ficticios na lista de clientes do banco
 		List<Cliente> ClientesBanco = new ArrayList<Cliente>();
 		ClientesBanco.add(cliente1);
 		ClientesBanco.add(cliente2);
+		ClientesBanco.add(cliente3);
+		ClientesBanco.add(cliente4);
 		
 		Gclientes = new GerenciadoraClientes(ClientesBanco);
 		
@@ -57,7 +63,6 @@ public class GerenciadoraClientestest {
 		
 		//verificacao
 		assertTrue(clienteRemovido);
-		assertThat(Gclientes.getClientesDoBanco().size(), Is.is(1));
 		assertNull(Gclientes.pesquisaCliente(idCliente2));
 	}
 	
@@ -70,21 +75,18 @@ public class GerenciadoraClientestest {
 	}
 	
 	@Test
-	public void deveVerificarValidadeDeIdade() throws IdadeNaoPermitidaException {
+	public void deveVerificarValidadeDeIdadeDentroDoIntervaloPermitido() throws IdadeNaoPermitidaException {
 		
-		Cliente cliente = new Cliente(5, "Pedro", 24, "Pedro@gmail.com", 19, true);
-		int idadeMenor = 11;
-		int idadeMaior = 70;
+		Cliente cliente = Gclientes.pesquisaCliente(idCliente3);
+		assertTrue(Gclientes.validaIdade(cliente.getIdade()));	
+	}
+	
+	@Test(expected = IdadeNaoPermitidaException.class)
+	public void deveVerificarValidadeDeIdadeForaDoIntervaloPermitido() throws IdadeNaoPermitidaException {
 		
-		//execucao
-		boolean idadeAceita = Gclientes.validaIdade(cliente.getIdade());
-		//boolean idadeInsuficiente = Gclientes.validaIdade(idadeMenor);
-		//boolean idadeExcedente = Gclientes.validaIdade(idadeMaior);
-		
-		//verificacao
-		assertTrue(idadeAceita);
-		//assertFalse(idadeInsuficiente);
-		//assertFalse(idadeExcedente);
-		
+		Cliente cliente1 = Gclientes.pesquisaCliente(idCliente4);
+		Cliente cliente2 = Gclientes.pesquisaCliente(idCliente4);
+		Gclientes.validaIdade(cliente1.getIdade());
+		Gclientes.validaIdade(cliente2.getIdade());
 	}
 }
